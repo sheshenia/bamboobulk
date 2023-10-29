aae087ce24c55ddfb24ea5346958674e29699f62
Pavlo Sheshenia <pavlo.sheshenia@ninjarmm.com>
2023-10-29 23:12:54 +0100

    do with hardcoded clock entries

--- extension/initial.js
+++ extension/initial.js
@@ -1,4 +1,74 @@
-const doBulk = () => {
+/*
+{
+  "entries": [
+    {
+      "id": null,
+      "trackingId": 1,
+      "employeeId": 405,
+      "date": "2023-10-26",
+      "start": "15:00",
+      "end": "19:00",
+      "note": "",
+      "projectId": null,
+      "taskId": null
+    },
+    {
+      "id": null,
+      "trackingId": 2,
+      "employeeId": 405,
+      "date": "2023-10-26",
+      "start": "20:00",
+      "end": "24:00",
+      "projectId": null,
+      "taskId": null
+    }
+  ]
+}
+* */
+
+const bambooURL = "https://ninjaone.bamboohr.com/timesheet/clock/entries"
+const doOneDay = async (employeeId, date, csrfToken) => {
+    const data = {
+        entries: [
+            {
+                "id": null,
+                "trackingId": 1,
+                employeeId,
+                date,
+                "start": "15:00",
+                "end": "19:00",
+                "note": "",
+                "projectId": null,
+                "taskId": null
+            },
+            {
+                "id": null,
+                "trackingId": 2,
+                employeeId,
+                date,
+                "start": "20:00",
+                "end": "24:00",
+                "projectId": null,
+                "taskId": null
+            }
+        ]
+    }
+    await fetch(bambooURL, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Csrf-Token": csrfToken,
+        },
+        body: JSON.stringify(data),
+    });
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const doBulk = async () => {
     const timesheetJsonEl = document.getElementById("js-timesheet-data")
     if (!timesheetJsonEl) return;
 
@@ -6,22 +76,35 @@ const doBulk = () => {
     console.log(timesheetJson)
 
     const dailyDetails = timesheetJson?.timesheet?.dailyDetails
-    if(!dailyDetails) return;
+    const employeeId = timesheetJson?.employeeId
+    const csrfToken = window.CSRF_TOKEN
+    console.log("CSRF_TOKEN: ", csrfToken)
+
+    if(!dailyDetails || !employeeId || !csrfToken) return;
     console.log(dailyDetails)
 
     const todayDayNum = new Date(timesheetJson?.today?.date ?? Date.now()).getDate()
     console.log(todayDayNum)
 
-    Object.values(dailyDetails).forEach((oneDay) => {
-        if(!oneDay.date || !!oneDay?.holidays?.length) return;
+    for (const oneDay of Object.values(dailyDetails)) {
+        //skipping Holidays
+        if(!oneDay.date || !!oneDay?.holidays?.length) continue;
 
         const oneDayDate = new Date(oneDay.date)
         const oneDayWeekNum = oneDayDate.getDay()
+
         //only days below or equal today, except weekends
-        if(oneDayDate.getDate() > todayDayNum || oneDayWeekNum === 6 || oneDayWeekNum === 0) return;
+        if(oneDayDate.getDate() > todayDayNum || oneDayWeekNum === 6 || oneDayWeekNum === 0) continue;
+
+        //skip days that already have clockEntries
+        if(!!oneDay?.clockEntries?.length) continue;
+
+        await doOneDay(employeeId, oneDay.date, csrfToken)
+        await delay(500)
 
         console.log(oneDay.date)
-    })
+    }
+    location.reload()
 }
 
 const timeSheetContainer = document.querySelector(".TimesheetSummaryContainer")
