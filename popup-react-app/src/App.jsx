3da729d1ba0766eaea1997a25ca55640cfdb8ff6
Pavlo Sheshenia <pavlo.sheshenia@ninjarmm.com>
2023-11-15 02:06:44 +0100

    custom days of week, clock entries

--- popup-react-app/src/App.jsx
+++ popup-react-app/src/App.jsx
@@ -1,13 +1,14 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 import {
-    CssBaseline,
+    CssBaseline, Divider, Fab, IconButton, Stack,
 } from "@mui/material";
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import {ClockEntry} from "./components/ClockEntry.jsx";
 import {getClockEntriesFromStorage, setClockEntriesToStorage} from "./utils/storage.js";
+import AddIcon from '@mui/icons-material/Add';
 
 const darkTheme = createTheme({
     palette: {
@@ -54,16 +55,30 @@ function App() {
         <ThemeProvider theme={darkTheme}>
             <CssBaseline/>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <h2 style={{textAlign: 'center'}}>BambooBulk Clock Entries</h2>
+                <Stack spacing={12}>
+                    <Stack
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={8}
+                    >
+                        <h2>BambooBulk Clock Entries</h2>
+                        <IconButton size="small" color="primary" aria-label="add" onClick={()=>console.log("clicked")}>
+                            <AddIcon />
+                        </IconButton>
+                    </Stack>
+                    {/*<h2 style={{textAlign: 'center'}}>BambooBulk Clock Entries</h2>*/}
 
-                {clockEntries.map(oneEntry => {
-                    return <ClockEntry
-                        clockEntry={oneEntry}
-                        key={oneEntry.id}
-                        updateEntry={onClockEntryUpdate}
-                    />
-                })}
-
+                    <Stack spacing={8} divider={<Divider orientation="horizontal" flexItem />}>
+                        {clockEntries.map(oneEntry => {
+                            return <ClockEntry
+                                clockEntry={oneEntry}
+                                key={oneEntry.id}
+                                updateEntry={onClockEntryUpdate}
+                            />
+                        })}
+                    </Stack>
+                </Stack>
             </LocalizationProvider>
         </ThemeProvider>
     )
--- popup-react-app/src/components/ClockEntry.jsx
+++ popup-react-app/src/components/ClockEntry.jsx
@@ -1,18 +1,26 @@
-import {Stack} from "@mui/material";
+import {Button, ButtonGroup, Divider, IconButton, Stack} from "@mui/material";
 import {TimePicker} from "@mui/x-date-pickers/TimePicker";
 import dayjs from "dayjs";
 import styled from "@emotion/styled";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const PickersContainer = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
     margin: 8px 0 !important;
     padding: 4px;
 `;
 
+const DayBtn = ({dayNum, isDaySelected, updateEntry}) => {
+    return <Button variant={isDaySelected ? "contained" : "outlined"}>dayjs().day(dayNum).format("ddd")</Button>
+}
+
 export const ClockEntry = ({clockEntry, updateEntry}) => {
-    return (<Stack spacing={4}>
-        <PickersContainer>
+    const isDayInEntry = (dayNum) => clockEntry.days.includes(dayNum)
+    const daySelected = (dayNum) => isDayInEntry(dayNum) ? "contained" : "outlined"
+
+    return (<Stack style={{marginLeft: "24px"}} spacing={4}>
+        <Stack direction="row" justifyContent="flex-start" spacing={2}>
             <TimePicker
                 label="Time Start"
                 value={dayjs('2022-04-17T' + clockEntry.start)}
@@ -24,7 +32,23 @@ export const ClockEntry = ({clockEntry, updateEntry}) => {
                 value={dayjs('2022-04-17T' + clockEntry.end)}
                 onChange={(newValue) => updateEntry({...clockEntry, end: formatHHMM(newValue)})}
             />
-        </PickersContainer>
+            <IconButton color="primary" aria-label="delete clock entry">
+                <DeleteIcon />
+            </IconButton>
+        </Stack>
+        <Stack direction="row" justifyContent="flex-start" spacing={20}>
+            <ButtonGroup variant="outlined" aria-label="Weekdays">
+                <Button variant={daySelected(1)}>Mon</Button>
+                <Button variant={daySelected(2)}>Tue</Button>
+                <Button variant={daySelected(3)}>Wed</Button>
+                <Button variant={daySelected(4)}>Thu</Button>
+                <Button variant={daySelected(5)}>Fri</Button>
+            </ButtonGroup>
+            <ButtonGroup variant="outlined" aria-label="Weekends">
+                <Button variant={daySelected(6)}>Sat</Button>
+                <Button variant={daySelected(0)}>Sun</Button>
+            </ButtonGroup>
+        </Stack>
     </Stack>)
 }
 
